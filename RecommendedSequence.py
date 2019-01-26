db836e44c3bcdaa34a76a111221db6a980a4cd11
Calvin <calvincruzader@gmail.com>
2019-01-25 19:15:31 -0600

    Adds problem statements into solutions

--- RecommendedSequence.py
+++ RecommendedSequence.py
@@ -4,8 +4,6 @@ class Solution:
 		ptrs = [0] * len(d)
 		d = list(d)
 
-
-
 		for c in s:
 			for i in range(len(ptrs)): 
 				# print(ptrs[i])
@@ -27,3 +25,18 @@ d = {"able", "ale", "apple", "bale", "kangaroo"}
 
 heyhye = Solution()
 print(heyhye.recommendedSequence(s,d))
+
+
+'''
+Given a string S and a set of words D, find the longest word in D that is a subsequence of S.
+
+Word W is a subsequence of S if some number of characters, possibly zero, can be deleted from S to form W, without reordering the remaining characters.
+
+Note: D can appear in any format (list, hash table, prefix tree, etc.
+
+For example, given the input of S = "abppplee" and D = {"able", "ale", "apple", "bale", "kangaroo"} the correct output would be "apple"
+
+The words "able" and "ale" are both subsequences of S, but they are shorter than "apple".
+The word "bale" is not a subsequence of S because even though S has all the right letters, they are not in the right order.
+The word "kangaroo" is the longest word in D, but it isn't a subsequence of S.
+'''
