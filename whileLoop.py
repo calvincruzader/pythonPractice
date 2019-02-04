c135b89dda560f1a94ba040883535ff79030d140
Calvin <calvincruzader@gmail.com>
2019-02-04 07:22:40 -0600

    adds while loop python nuances

--- whileLoop.py
+++ whileLoop.py
@@ -9,9 +9,17 @@ def testWhileElse(x, val_to_find):
 		idx += 1
 	else: 
 		x.append(val_to_find)
-		print("{} was not in the original array of {}".format(val_to_find, x))
+		print("I'm in the else loop and {} was not in the original array of {}".format(val_to_find, x))
+
+	print("always run this string.")
 
 print("else statements in loops only execute if the loop terminated without breaks.")
 val_to_find = 4
 print("val_to_find = {}".format(val_to_find))
-print()
+testWhileElse(x, val_to_find)
+
+print('-----')
+
+val_to_find = 2
+print("val_to_find = {}".format(val_to_find))
+testWhileElse(x, val_to_find)
